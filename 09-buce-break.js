fe960acd711a6d4900bc06d2f44101ed16dc6110
hy <henrryyanez@gmail.com>
2019-05-18 16:09:58 -0300

    Uso del Break con While

---
+++ 09-buce-break.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// Bucle While
+
+var year = 2018;
+
+while (year != 1991) {
+  // Ejecute esto:
+  console.log("Estamos en el año "+year);
+  
+  if (year == 2000) {
+    break;
+  }
+  year--;
+}
