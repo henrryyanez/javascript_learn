28c40215a69437eb8ea5a3686e8afc5284caf68f
hy <henrryyanez@gmail.com>
2019-05-18 15:00:10 -0300

    Ejercicios con Switch

---
+++ switch.js
@@ -0,0 +1,25 @@
+'use strict'
+//Uso de switch
+// console.log('switch');
+
+var edad = 33;
+var imprime = "";
+
+switch (edad) {
+  case 18:
+    imprime = "Acabas de cumplir la mayoria de edad"
+    break;
+    case 25:
+      imprime = "Ya eres un adulto"
+    break;
+    case 40:
+      imprime = "Crisis de los 40"
+    break;
+    case 75:
+      imprime = "Eres un anciano"
+    break;
+    default:
+      imprime = 'Tu edad es neutra'
+}
+
+console.log(imprime);
