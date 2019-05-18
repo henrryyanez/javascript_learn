29f9f157d3662979dce30ccbdfaa9a355bad6227
hy <henrryyanez@gmail.com>
2019-05-18 15:18:17 -0300

    Probando el bucle for y haciendo Debugger

---
+++ 08-bucle-for.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Bucle FOR
+// Bucle es una estructura de control que se repite varias veces
+
+var numero = 100;
+
+for (var i = 0; i < numero; i++) {
+  console.log("Vamos por el numero "+i);
+  debugger;
+}
+
+
+  // Con el debugger podemos hacer un seguimiento
+  // del comportamiento y recorrido de la funcion
+  // de tal forma que vamos al navegador en watch
+  // y pasamos las variables "numero" y "i"
