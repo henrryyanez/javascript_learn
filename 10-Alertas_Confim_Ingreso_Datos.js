a06adcea20b6f999a148314d2ac5a075284396d6
hy <henrryyanez@gmail.com>
2019-05-18 21:05:09 -0300

    Alertas y confirmaciones con ingreso de datos

---
+++ 10-Alertas_Confim_Ingreso_Datos.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// ALERTA
+alert("Esta es mi alerta!");
+
+// ALERTA DE CONFIRMACIÓN usando confirm
+var mi_resultado = confirm("¿Estas seguro de querer continuar?");
+console.log(mi_resultado);  //Al hacer esto nos devuelve un booleano True ó False
+
+// INGRESO DE DATOS USANDO prompt
+var mi_resultado2 = prompt("¿Que edad tienes?", 18);
+console.log(typeof mi_resultado2);
+
+// Con el typeof puedo ver que tipo de datos es lo registrado
