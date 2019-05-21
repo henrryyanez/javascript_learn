7681389bed810e6658bd262169463818ec07c526
hy <henrryyanez@gmail.com>
2019-05-21 13:33:17 -0300

    Probando funciones

---
+++ 19-funciones.js
@@ -0,0 +1,21 @@
+'use script'
+
+// funciones
+/*
+Una función es una agrupación reutilizable de un conjunto
+de instrucciones
+*/
+
+// Defino la funcion
+function Calculadora(){
+  // Conjunto de instrucciones a Ejecutar
+  console.log("Hola soy la Calculadora");
+  console.log("si soy yo");
+  return "Hola soy la Calculadora!!";
+}
+
+// Invocar o llamar a la funcion
+Calculadora();
+var resultado = Calculadora();
+
+console.log(resultado);
