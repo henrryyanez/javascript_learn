d24d0614f9a3a7f503f228d5369da0c3efa0f379
hy <henrryyanez@gmail.com>
2019-05-22 12:56:09 -0300

    Usando funciones anonimas

---
+++ 21-funciones-anonimas.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Funciones anonimas+
+// Es una funcion que no tiene nombre
+
+// var pelicula = function (nombre) {
+//     return "La pelicula es: "+nombre;
+// }
+
+// callback: es una funcion que se ejecuta despues de otra
+
+function sumame(numero1, numero2, sumaYmuestra, sumaPorDos) {
+  var sumar = numero1 + numero2;
+
+  sumaYmuestra(sumar);
+  sumaPorDos(sumar);
+  return sumar;
+}
+
+sumame(5, 7, function(dato) {
+    console.log("La suma es: ", dato);
+},
+  function(dato) {
+    console.log("La suma por dos es: ", (dato*2));
+});
