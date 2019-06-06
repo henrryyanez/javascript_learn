6f39870f56d31510ea7d3f690079735a96e546d4
hy <henrryyanez@gmail.com>
2019-06-05 21:19:22 -0300

    El ambito de las variables

---
+++ ambito.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var texto = "Variable global";
+var numero = 12;
+
+function holaMundo(texto){
+  var hola_mundo = "Texto dentro de funcion"
+
+  console.log(texto);
+  console.log(numero);
+  console.log(hola_mundo);
+}
+
+holaMundo(texto);
